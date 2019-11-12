7475d6a77b504701724a66f616ebaf599383891f
Jashwanthi928 <54614090+Jashwanthi928@users.noreply.github.com>
2019-11-12 12:47:36 +0530

    updated to class

--- app.js
+++ app.js
@@ -1,40 +1,40 @@
 //  This is an updated version of weather app which uses google places api and open weather api to display weather.
-// Author
-// Jashwanthi 
+// Author: Jashwanthi
 
 let dataBox = 0;
 
 //Code to display day and time
-
-
-function getTwelveHrs() {
-  var today = new Date();
-  var day = today.getDay();
-  var hourlist = [12,01,02,03,04,05,06,07,08,09,10,11,12,01,02,03,04,05,06,07,08,09,10,11];
-  var daylist = [
-    "Sunday",
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday"
-  ];
-  var hour = today.getHours();
-  var minute = today.getMinutes();
-  var append = hour >= 12 ? " pm " : " am ";
-  if (minute < 10) {
-    minute = "0" + minute;
-  } else {
-    minute = minute;
+class GetTwelveHrs {
+  time() {
+    var today = new Date();
+    var day = today.getDay();
+    var hourlist = [12,1,2,3,4,5,6,7,8,9,10,11,12,1,2,3,4,5,6,7,8,9,10,11];
+    var daylist = [
+      "Sunday",
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday"
+    ];
+    var hour = today.getHours();
+    var minute = today.getMinutes();
+    var append = hour >= 12 ? " pm " : " am ";
+    if (minute < 10) {
+      minute = "0" + minute;
+    } else {
+      minute = minute;
+    }
+    var time = hourlist[hour] + ":" + minute + append;
+    var day = daylist[day];
+    document.getElementById("daytime").innerHTML = day + "," + " " + time;
   }
-  var time = hourlist[hour] + ":" + minute + append;
-  var day = daylist[day];
-  document.getElementById("daytime").innerHTML = day + "," + " " + time;
 }
+let weather = new GetTwelveHrs();
+setInterval(weather.time, 1000);
 
 // code to display weather
-
 function weatherDetails() {
   let cityName = document.getElementById("input").value;
   const key = "3b3c21860d9f02f48d6de97dcfb3fc4e";
@@ -47,10 +47,11 @@ function weatherDetails() {
   )
     .then(function(resp) {
       return resp.json();
-    }) // Convert data to json
+    })
 
     .then(function(d) {
       drawWeather(d);
+      console.log(d);
     })
     .catch(function() {});
 }
@@ -73,7 +74,6 @@ function drawWeather(d) {
 }
 
 //Temperature conversion
-
 function convertTemp(value) {
   let celcius = Math.round(parseFloat(dataBox.main.temp) - 273.15);
   let fahrenheit = Math.round(
